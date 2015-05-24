3a55b22c03ae55b55e3d240da0d73e13c1550701
arash-sadr <arash.sdr@gmail.com>
2015-05-24 12:59:43 +0800

    ExactDocBox 1.0.2
    After Authentication Account data retrieved.

--- router/main.js
+++ router/main.js
@@ -1,3 +1,6 @@
+var dbox = require("dbox");
+var dapp = dbox.app({"app_key":"4gcbtkrnpk8rxxq","app_secret":"r8moojorlfug6u7"});
+
 module.exports=function(app)
 {
 app.get('/',function(req,res){
@@ -6,12 +9,22 @@ res.render('index.html');
 
 app.get('/drobboxCall',function(req,res){
 
-var dbox = require("dbox");
-var dapp = dbox.app({"app_key":"4gcbtkrnpk8rxxq","app_secret":"r8moojorlfug6u7"});
-
 dapp.requesttoken(function(status,request_token){
-	res.redirect(request_token.authorize_url);	
-	}) 
+	res.redirect(request_token.authorize_url+'&oauth_callback=http://localhost:3000/dropboxCallBack');	
+	console.log(request_token.authorize_url);
+	requestToken = request_token;	
+  }); 
+});
+
+app.get('/dropboxCallBack',function(req,res){
+  dapp.accesstoken(requestToken, function(status, access_token){
+  console.log(access_token);
+  res.render('dropboxCallBack.html');
+  var client = dapp.client(access_token);
+   client.account(function(status, reply){
+   console.log(reply)
+   });
+  }); 
 });
 
 app.get('/about',function(req,res){
